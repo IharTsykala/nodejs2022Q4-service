b96d38ba7b5bd42be12a1aa2d9965af0cebb1a0f
IharTsykala <ihatsykala@kitrum.com>
2023-02-05 19:30:14 +0600

    feat: swagger and readme

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 ## Downloading
 
 ```
-git clone {repository URL}
+git clone https://github.com/IharTsykala/nodejs2022Q4-service
 ```
 
 ## Installing NPM modules
@@ -24,7 +24,7 @@ npm start
 ```
 
 After starting the app on port (4000 as default) you can open
-in your browser OpenAPI documentation by typing http://localhost:4000/doc/.
+in your browser OpenAPI documentation by typing http://localhost:4000/swagger/.
 For more information about OpenAPI/Swagger please visit https://swagger.io/.
 
 ## Testing
@@ -65,8 +65,3 @@ npm run lint
 npm run format
 ```
 
-### Debugging in VSCode
-
-Press <kbd>F5</kbd> to debug.
-
-For more information, visit: https://code.visualstudio.com/docs/editor/debugging
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ConfigModule } from '@nestjs/config';
 ConfigModule.forRoot();
 
@@ -8,7 +9,19 @@ const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Nodejs2022Q4-service example')
+    .setDescription('The nodejs2022Q4-service API description')
+    .setVersion('1.0')
+    .addTag('nodejs2022Q4-service')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(PORT);
 }
 bootstrap().then();
